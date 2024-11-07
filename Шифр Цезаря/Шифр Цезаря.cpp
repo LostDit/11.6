9e1fd1bb2b5f189e464d3c4ce6fe434694711047
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-11-07 21:35:55 +0300

    #Проверка отправки

--- Шифр Цезаря/Шифр Цезаря.cpp
+++ Шифр Цезаря/Шифр Цезаря.cpp
@@ -13,6 +13,18 @@ static std::string encrypt_caesar(std::string stroka) {
 	return stroka;
 };
 
+static std::string encrypt_caesar(std::string stroka) {
+
+	std::cin >> stroka;
+	for (int i = 0; i < stroka.length(); i++) {
+		if (stroka[i] < 'A' && stroka[i]>'Z' || stroka[i] < 'a' && stroka[i]>'z') {
+			
+		}else
+			stroka[i] = (char)stroka[i] + 3;
+	}
+	return stroka;
+};
+
 int main() {
 	std::string stroka;
 	stroka = encrypt_caesar(stroka);
