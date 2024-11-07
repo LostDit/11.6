29303e7011927843e91804721c52358336e3b1de
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-11-07 21:35:00 +0300

    Проверка выгрузки

--- Шифр Цезаря/Шифр Цезаря.cpp
+++ Шифр Цезаря/Шифр Цезаря.cpp
@@ -1,10 +1,20 @@
 ﻿#include <iostream>
-static void encrypt_caesar(std::string stroka) {
+#include <string>
 
+static std::string encrypt_caesar(std::string stroka) {
 
+	std::cin >> stroka;
+	for (int i = 0; i < stroka.length(); i++) {
+		if (stroka[i] < 'A' && stroka[i]>'Z' || stroka[i] < 'a' && stroka[i]>'z') {
+			
+		}else
+			stroka[i] = (char)stroka[i] + 3;
+	}
+	return stroka;
 };
 
 int main() {
-
-
+	std::string stroka;
+	stroka = encrypt_caesar(stroka);
+	std::cout << stroka;
 }
