f6833d801d1f58d18fea6e68c68f1c89342455d3
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-11-08 22:15:45 +0300

    #Добавил ввод переменной типа int

--- Шифр Цезаря/Шифр Цезаря.cpp
+++ Шифр Цезаря/Шифр Цезаря.cpp
@@ -1,32 +1,22 @@
 ﻿#include <iostream>
 #include <string>
-
-static std::string encrypt_caesar(std::string stroka) {
-
-	std::cin >> stroka;
+std::string encrypt_caesar(std::string stroka) {
+	int g;
+	getline(std::cin, stroka);
+	std::cout << "Code: ";
+	std::cin >> g;
 	for (int i = 0; i < stroka.length(); i++) {
-		if (stroka[i] < 'A' && stroka[i]>'Z' || stroka[i] < 'a' && stroka[i]>'z') {
-			
-		}else
-			stroka[i] = (char)stroka[i] + 3;
-	}
-	return stroka;
-};
-
-static std::string encrypt_caesar(std::string stroka) {
-
-	std::cin >> stroka;
-	for (int i = 0; i < stroka.length(); i++) {
-		if (stroka[i] < 'A' && stroka[i]>'Z' || stroka[i] < 'a' && stroka[i]>'z') {
-			
-		}else
-			stroka[i] = (char)stroka[i] + 3;
+		if ((stroka[i] >= 'A' && stroka[i]<='Z') || (stroka[i] >= 'a' && stroka[i]<='z')) {
+			stroka[i] = (char)stroka[i] + g;
+		}
+		
 	}
 	return stroka;
 };
 
 int main() {
 	std::string stroka;
+	std::cout << "Enter the text: ";
 	stroka = encrypt_caesar(stroka);
 	std::cout << stroka;
 }
