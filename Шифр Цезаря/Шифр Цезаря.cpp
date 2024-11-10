0e3e5da987afbb642f90943ac83a107a1571b4fb
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-11-10 07:33:15 +0300

    #Оказалось всё проще

--- Шифр Цезаря/Шифр Цезаря.cpp
+++ Шифр Цезаря/Шифр Цезаря.cpp
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿//Корректность работы программы по созданию зашифрованной версии слова или текста.
+
+#include <iostream>
 #include <string>
 std::string encrypt_caesar(std::string stroka) {
 	int g;
@@ -6,10 +8,16 @@ std::string encrypt_caesar(std::string stroka) {
 	std::cout << "Code: ";
 	std::cin >> g;
 	for (int i = 0; i < stroka.length(); i++) {
-		if ((stroka[i] >= 'A' && stroka[i]<='Z') || (stroka[i] >= 'a' && stroka[i]<='z')) {
-			stroka[i] = (char)stroka[i] + g;
+		if ((stroka[i] >= 'A' && stroka[i] <= 'Z') || (stroka[i] >= 'a' && stroka[i] <= 'z')) {
+			if (stroka[i] <= 'Z') {
+				
+				stroka[i] = 65+(stroka[i] - 65 + g) % 26;
+			
+			}else {
+				stroka[i] = 97+(stroka[i] - 97 + g) % 26;
+			}
 		}
-		
+
 	}
 	return stroka;
 };
