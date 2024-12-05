b75b2ee7d3ab351e961a0e47ea07f8a9ba8586f3
Maksim <maksim.antonov2012@yandex.ru>
2024-12-05 12:46:32 +0300

    Почти тут доделал
    Нужно будет допилить точки и нули которые могут идти подряд

--- Валидация IP-адреса/Валидация IP-адреса.cpp
+++ Валидация IP-адреса/Валидация IP-адреса.cpp
@@ -1,46 +1,62 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
-bool _digit(std::string ipvalidation, bool valid) { //проверка на то что только цифры в строке
-	for (int i = 0; i < ipvalidation.length() && valid == true; i++) {
-		if (ipvalidation[i] < '0' || ipvalidation[i]>'9'){
-			ipvalidation[i] == '.' ? valid = true : valid = false;
-			continue;
-		}else valid = true;
-	}
-	return valid;
-};
-bool _lengthdigit(std::string ipvalidation,bool lengthdigit) { //проверка на длину цифр между точками
-	for (int i = 0; i < ipvalidation.length()&& lengthdigit == true; i++) {
-		
 
-	}
-};
-bool _tochka(std::string ipvalidation, bool tochka) { //проверка на точки
-	int tochka = 0;
+bool _tochka(std::string ipvalidation, bool tochka) {
+	int t = 0;
 	for (int i = 0; i < ipvalidation.length() && tochka == true; i++) {
-		if (ipvalidation[i] == '.'&&tochka>3) {
-			tochka = false;
-			continue;
-		}else if (ipvalidation[i] == '.') {
-			tochka++;
-		}else if (ipvalidation[i]=='.') {
-
-		}else tochka = true;
-	}
+		if (ipvalidation[i] == '.') {
+			t++;
+		}else continue;
+	} if (ipvalidation[0] == '.') {
+		tochka = false;
+	}else if (t == 3) {
+		tochka = true;
+	}else tochka = false;
 	return tochka;
-};
-bool _valid(std::string ipvalidation,bool valid) {
-	bool tochka = true, lengthdigit =true;
-	valid = _digit(ipvalidation, valid);	//проверка на то что только цифры в строке
-	tochka = _tochka(ipvalidation, tochka);	//проверка на точки
-	lengthdigit = _lengthdigit(ipvalidation, lengthdigit);	//проверка на длину цифр между точками
+}
+
+bool _digit(std::string ipvalidation, bool digit) {
+	for (int i = 0; i < ipvalidation.length()&&digit==true;i++) {
+		if (ipvalidation[i] > '9' || ipvalidation[i] < '0') {
+			if (ipvalidation[i] == '.') {
+				digit = true;
+				continue;
+			}digit = false;
+			continue;
+		}
+	}
+	return digit;
+}
+bool _sizedigits(std::string ipvalidation, bool sizedigits) {
+	bool chekdigitssize = true;std::string sizedi; int sd = 0;
+	chekdigitssize = _digit(ipvalidation, chekdigitssize);
+	for (int i = 0; i < ipvalidation.length() && sizedigits == true;i++) {
+		if (ipvalidation[i] == '.') {
+			sd = stoi(sizedi);
+			
+			if (sd < 0 || sd>=256) {
+				sizedigits = false;
+				continue;
+			}else sizedi = "";
+			
+			continue;                                           
+		}else if (chekdigitssize == true) {
+			sizedi += ipvalidation[i];
+		}
+	
+	}
+	return sizedigits;
+}
+
+bool _valid(std::string ipvalidation, bool valid) {
+	valid = _digit(ipvalidation, valid)&&_tochka(ipvalidation,valid)&&_sizedigits(ipvalidation,valid);
 	return valid;
-};
+}
 int main() {
-	std::string ipvalidation;
-	bool valid = true;
-	std::cout << "Enter IPv4: ";
+	std::string ipvalidation;bool valid = true;
+	std::cout << "Enter the IPv4: ";
 	getline(std::cin, ipvalidation);
 	valid = _valid(ipvalidation, valid);
 	valid == true ? std::cout << "Valid" : std::cout << "Invalid";
+
 }
