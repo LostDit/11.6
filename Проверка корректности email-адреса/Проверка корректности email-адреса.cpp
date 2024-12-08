afbc03e1ad5104c06ee9d58ab5263e590b305d13
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-12-08 20:09:52 +0300

    Доделано

--- Проверка корректности email-адреса/Проверка корректности email-адреса.cpp
+++ Проверка корректности email-адреса/Проверка корректности email-адреса.cpp
@@ -1,15 +1,33 @@
 ﻿#include <iostream>
 #include <string>
-#include<vector>
-std::string WhiteList = "!#$%&'*+-/=?^_`{|}~";
 using namespace std;
-bool CheckMailBodyValid(string body) {
+
+bool ValidString(string body, string WhiteList, int maxlen) {
+
+    if (body.length() > maxlen || body.length() == 0)
+        return false;
+
+    for (int i = 0; i < body.length(); i++) {
+        if (!((body[i] >= '0' && body[i] <= '9') || (body[i] >= 'a' && body[i] <= 'z') || (body[i] >= 'A' && body[i] <= 'Z'))) {
+            bool IsCorect = false;
+            for (int j = 0; j < WhiteList.length(); j++) {
+                if (body[i] == WhiteList[j])
+                {
+                    IsCorect = true;
+                    break;
+                }
+            }
+            if (!IsCorect)
+            {
+                return false;
+            }
+        }
+    }
     return true;
 }
-bool CheckMailDomainValid(string domain) {
-    return true;
-}
+
 bool CheckMailValid(std::string email) {
+
     std::string BodyValid, DomainValid;
     int sobakaIndex = -1;
     bool isdotfind = false;
@@ -19,26 +37,32 @@ bool CheckMailValid(std::string email) {
     if (email[0] == '@' && email[email.length() - 1] == '@')
         return false;
     for (int i = 0; i < email.length() - 1; i++) {
-        
-        if(email[i] == '.' && email[i+1] == '.'){
+
+        if (email[i] == '.' && email[i + 1] == '.') {
             return false;
         }
 
-        if(email[i] == '@') {
-            if(sobakaIndex == -1)
+        if (email[i] == '@') {
+            if (sobakaIndex == -1)
                 sobakaIndex = i;
             else
                 return false;
         }
     }
-
-    return  CheckMailBodyValid(email.substr(0, sobakaIndex - 1)) && CheckMailDomainValid(email.substr(sobakaIndex + 1, email.length() - 1));
+    if (sobakaIndex == -1) {
+        return false;
+    }
+    return  ValidString(email.substr(0, sobakaIndex), "!#$%&'*+-/=?^_`{|}~.", 64) && ValidString(email.substr(sobakaIndex + 1, email.length() - 1), ".-", 63);
 
 }
 
 int main() {
     std::string email;
-    std::cout << "Enter the email: ";
-    getline(std::cin, email);
-    CheckMailValid(email) ? std::cout << "Yes" : std::cout << "No";
+    while (true) {
+        std::cout << "Enter the email: ";
+        getline(std::cin, email);
+        system("cls");
+        std::cout << email << "\n";
+        CheckMailValid(email) ? std::cout << "Yes\n" : std::cout << "No\n";
+    }
 }
